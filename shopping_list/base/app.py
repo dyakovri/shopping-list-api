76970013d0c56be62cf2ce139e00f7a12f9b6800
Roman Dyakov <dyakovri@gmail.com>
2021-09-07 16:59:58 +0300

    DB Middleware

--- shopping_list/base/app.py
+++ shopping_list/base/app.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
+from fastapi_sqlalchemy import DBSessionMiddleware
 from shopping_list import __version__
-from shopping_list.api import item_router, list_router, good_router
+from shopping_list.api import good_router, item_router, list_router
+from shopping_list.base.settings import get_settings
+
+settings = get_settings()
 
 
 def create_app() -> FastAPI:
@@ -12,5 +16,11 @@ def create_app() -> FastAPI:
     app.include_router(list_router, prefix='/lists', tags=['List'])
     app.include_router(item_router, prefix='/lists/{list_id}/items', tags=['Item'])
     app.include_router(good_router, tags=['Recommendations'])
+    app.add_middleware(
+        middleware_class=DBSessionMiddleware,
+        db_url=settings.DB_DSN,
+        engine_args={'pool_pre_ping': True},
+        session_args={'autocommit': False, 'autoflush': False},
+    )
     return app
 
